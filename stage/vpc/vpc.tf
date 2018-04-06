0d9e2d48760f754b405fca70ead57bbaf890ea60
eluts <eluts15@gmail.com>
2018-04-05 20:54:10 -0700

    add template for frontend client facing web server and backend database tiers

--- stage/vpc/vpc.tf
+++ stage/vpc/vpc.tf
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "default" {
 resource "aws_subnet" "us-west-1a-public-staging" {
     vpc_id = "${aws_vpc.default.id}"
     
-    cidr_block = "${var.public_subnet_cidr}"
+    cidr_block = "${var.public_subnet_cidr_staging}"
     availability_zone = "us-west-1a"
 
     tags {
@@ -27,7 +27,7 @@ resource "aws_route_table" "us-west-1a-public-staging" {
     vpc_id = "${aws_vpc.default.id}"
     
     route {
-      cidr_block = "${var.public_subnet_cidr}"
+      cidr_block = "${var.public_subnet_cidr_staging}"
       gateway_id = "${aws_internet_gateway.default.id}"
     }
 
@@ -40,7 +40,7 @@ resource "aws_route_table" "us-west-1a-public-staging" {
 resource "aws_subnet" "us-west-1a-private-staging" {
     vpc_id = "${aws_vpc.default.id}"
 
-    cidr_block = "${var.private_subnet_cidr}"
+    cidr_block = "${var.private_subnet_cidr_staging}"
 
     tags {
         Name = "Private Subnet Staging"
@@ -51,7 +51,7 @@ resource "aws_route_table" "us-west-1a-private-staging" {
     vpc_id = "${aws_vpc.default.id}"
 
     route {
-        cidr_block = "${var.private_subnet_cidr}"
+        cidr_block = "${var.private_subnet_cidr_staging}"
         instance_id = "${aws_instance.nat.id}"
     }
 
